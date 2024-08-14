70f1cc2989759324acfbe0965732516ebf206fcd
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-08-14 14:17:15 -0300

    Update: fix bug gerenciamento de roupas

--- client/framework/qb/main.lua
+++ client/framework/qb/main.lua
@@ -49,9 +49,12 @@ function Framework.IsPlayerAllowed(citizenid)
 end
 
 function Framework.GetRankInputValues(type)
-    local grades = QBCore.Shared.Jobs[client.job.name].grades
+    local JOBS = exports.qbx_core:GetJobs()
+    local GANGS = exports.qbx_core:GetGangs()
+
+    local grades = JOBS[client.job.name].grades
     if type == "gang" then
-        grades = QBCore.Shared.Gangs[client.gang.name].grades
+        grades = GANGS[client.gang.name].grades
     end
     return getRankInputValues(grades)
 end
